96461c210d03629361f8ed711681b10671cd3df0
Saptarshi <saptarshichakraborty04@outlook.com>
2022-04-14 21:59:58 +0530

    First Commit

---
+++ lib/details.dart
@@ -0,0 +1,367 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'coin_logos.dart';
+import 'package:http/http.dart';
+
+class CoinDetails extends StatefulWidget {
+  @override
+  State<CoinDetails> createState() => _CoinDetailsState();
+}
+
+class _CoinDetailsState extends State<CoinDetails> {
+  var data;
+  late String name = '';
+  double change = 0.0;
+  dynamic price = 0, highestPrice = 0, lowestPrice = 0, marketCapital = 0;
+
+  Future getCoinDetails(String iso) async {
+    // print('getCoinDetails ($iso)\n');
+    Response response = await get(Uri.parse(
+        'https://production.api.coindesk.com/v2/tb/price/ticker?assets=$iso'));
+    var responseBody = await jsonDecode(response.body);
+    if (responseBody == null) return;
+    var data = responseBody['data'][iso];
+
+    // print('iso [$iso]');
+    // print("name [${data['name']}]");
+
+    setState(() {
+      name = data['name'];
+      price = data['ohlc']['c'];
+      highestPrice = data['ohlc']['h'];
+      lowestPrice = data['ohlc']['l'];
+      change = data['change']['percent'];
+      marketCapital = data['marketCap'];
+    });
+  }
+
+  @override
+  void didChangeDependencies() {
+    data = ModalRoute.of(context)?.settings.arguments;
+    // print(data['iso']);
+    getCoinDetails(data['iso']);
+
+    super.didChangeDependencies();
+  }
+
+  void MyCallback() {
+    // print('Mycallback() Called');
+    getCoinDetails(data['iso']);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // print('\nBUILD START\n');
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0,
+        title: Text(
+          '(${data['iso']}) Price Details',
+          style: TextStyle(
+            color: Colors.tealAccent,
+            fontWeight: FontWeight.w500,
+            fontFamily: 'Ubuntu',
+            // fontSize: 20.0,
+          ),
+        ),
+      ),
+      body: DetailsCard(
+        iso: data['iso'],
+        name: name,
+        price: price,
+        highestPrice: double.parse((highestPrice).toStringAsFixed(2)),
+        lowestPrice: double.parse((lowestPrice).toStringAsFixed(2)),
+        change: double.parse((change).toStringAsFixed(2)),
+        marketCapital: marketCapital,
+        callback: MyCallback,
+        // iso: 'BTC',
+        // name: 'Bitcoin',
+        // price: 41066.05,
+        // highestPrice: 41378.3,
+        // lowestPrice: 39275.8,
+        // change: double.parse((change).toStringAsFixed(2)),
+        // // change: 2.3192858139488677,
+        // marketCapital: 780663236090.3649,
+      ),
+    );
+  }
+}
+
+class DetailsCard extends StatelessWidget {
+  final String name, iso;
+  final dynamic price, highestPrice, lowestPrice, marketCapital, change;
+  final VoidCallback callback;
+
+  const DetailsCard({Key? key, 
+    required this.iso,
+    required this.name,
+    this.price,
+    this.highestPrice,
+    this.lowestPrice,
+    this.change,
+    this.marketCapital,
+    required this.callback,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    String? imgPath = '';
+    imgPath = logoPath[iso];
+    var priceColor =
+        (change > 0) ? Colors.greenAccent[400] : Colors.redAccent[400];
+    var changeColor = (change > 0) ? Colors.green[700] : Colors.redAccent[700];
+    var changeIcon = (change > 0) ? Icons.arrow_drop_up : Icons.arrow_drop_down;
+    // String priceChange = (change > 0) ? 'Large' : 'Small';
+    // print('pricechange percent $change');
+    // print('priceChange is = $priceChange');
+    String marketCap = '';
+    double a;
+    if (marketCapital > 1000000000) {
+      a = double.parse((marketCapital / 1000000000).toStringAsFixed(2));
+      marketCap = '$a B';
+    } else if (marketCapital > 1000000) {
+      a = double.parse((marketCapital / 1000000).toStringAsFixed(2));
+      marketCap = '$a M';
+    } else if (marketCapital > 1000) {
+      a = double.parse((marketCapital / 1000).toStringAsFixed(2));
+      marketCap = '$a K';
+    } else {
+      a = double.parse((marketCapital).toStringAsFixed(2));
+      marketCap = '$a';
+    }
+
+    return Container(
+      color: Colors.black,
+      padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Coin Details Container
+          Container(
+            // color: Colors.blueGrey[100],
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  // backgroundColor: Colors.white,
+                  backgroundImage: AssetImage(imgPath!),
+                  radius: 32.0,
+                  // child: CircleAvatar(
+                  //   radius: 30.0,
+                  //   backgroundImage: AssetImage('images/logo/default.png'),
+                  // ),
+                ),
+                SizedBox(width: 20.0),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      '($iso)',
+                      style: TextStyle(
+                        color: Colors.amber[300],
+                        fontWeight: FontWeight.w500,
+                        fontSize: 24.0,
+                      ),
+                    ),
+                    SizedBox(height: 8.0),
+                    Text(
+                      name,
+                      // 'Basic Attention Token',
+                      style: TextStyle(
+                        color: Colors.amber[400],
+                        fontWeight: FontWeight.w500,
+                        letterSpacing: 1.0,
+                        fontSize: 22.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+
+          SizedBox(height: 15.0),
+
+          // Coin Price
+          Text(
+            '\$$price',
+            style: TextStyle(
+              // color: Colors.redAccent[400],
+              color: priceColor,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 1.0,
+              fontSize: 40.0,
+            ),
+          ),
+
+          // Price Change Box
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                'Change in 24Hr : ',
+                style: TextStyle(color: Colors.grey[500]),
+              ),
+              Icon(
+                changeIcon,
+                color: changeColor,
+                size: 30.0,
+              ),
+              Text(
+                '${change}%',
+                style: TextStyle(
+                  color: changeColor,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
+          const Divider(
+            height: 30.0,
+            thickness: 2.0,
+            color: Colors.white,
+          ),
+
+          const SizedBox(height: 10.0),
+
+          // Highest Price
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Highest(24Hr)',
+                style: TextStyle(
+                  color: Colors.blue[100],
+                  fontSize: 18.0,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(2, 0, 6, 0),
+                child: Icon(
+                  Icons.keyboard_double_arrow_right_rounded,
+                  color: Colors.blue[100],
+                ),
+              ),
+              Text(
+                '\$$highestPrice',
+                style: TextStyle(
+                  color: Colors.amber[300],
+                  fontWeight: FontWeight.w600,
+                  fontSize: 24.0,
+                ),
+              ),
+            ],
+          ),
+
+          const SizedBox(height: 10.0),
+
+          // Lowest Price
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                '\$$lowestPrice',
+                style: TextStyle(
+                  color: Colors.amber[300],
+                  fontWeight: FontWeight.w600,
+                  fontSize: 24.0,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(2, 0, 6, 0),
+                child: Icon(Icons.keyboard_double_arrow_left_rounded,
+                    color: Colors.blue[100]),
+              ),
+              Text(
+                'Lowest(24Hr)',
+                style: TextStyle(
+                  color: Colors.blue[100],
+                  fontSize: 18.0,
+                ),
+              ),
+            ],
+          ),
+
+          // Last
+          const SizedBox(height: 20.0),
+
+          const Center(
+            child: Text(
+              'Market Capital (approx.)',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20.0,
+              ),
+            ),
+          ),
+
+          SizedBox(height: 3.0),
+
+          Center(
+            child: Text(
+              '\$$marketCap',
+              style: TextStyle(
+                color: Colors.orange[800],
+                fontSize: 30.0,
+                fontFamily: 'Ubuntu',
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ),
+
+          Divider(height: 30.0, thickness: 2.0, color: Colors.white),
+
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.tealAccent),
+                        // fixedSize: MaterialStateProperty.all(),
+                      ),
+                      onPressed: () {
+                        // print('Refresh Price');
+                        callback();
+                      },
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.refresh,
+                            size: 25.0,
+                            color: Colors.brown[800],
+                          ),
+                          SizedBox(width: 2.0),
+                          Text(
+                            'Refresh',
+                            style: TextStyle(
+                              color: Colors.brown[800],
+                              fontSize: 25.0,
+                              fontFamily: 'Ubuntu',
+                              // letterSpacing: 1.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,258 @@
+// import 'package:cryptoprice/_getAllCoin.dart';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'details.dart';
+import 'coin_logos.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "My Title",
+      // initialRoute: '/details',
+      routes: {'/details': (context) => CoinDetails()},
+      scrollBehavior: const ScrollBehavior(
+          androidOverscrollIndicator: AndroidOverscrollIndicator.glow),
+      // home: Home(),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class Coins {
+  var item;
+  Coins(this.item);
+  @override
+  String toString() {
+    return '{ $item }';
+  }
+}
+
+class _HomeState extends State<Home> {
+  var _data;
+  int _noOfItem = 0;
+
+  void getData() async {
+    Response response = await get(Uri.parse(
+        'https://production.api.coindesk.com/v2/tb/price/ticker?assets=BTC,CHZ,SUSHI,XYO,ENJ,COTI,LPT,ANKR,IOTX,COMP,CELO,NKN,KSM,RLC,SKL,KEEP,ICP,SOL,WBTC,XRP,FIL,DOT,BAT,MATIC,BSV,XLM,MLN,KAVA,XEM,LSK,QTUM,DCR,DNT,DAI,ANT,REP,BAND,NMR,PAX,STORJ,CVC,BNT,NU,LTC,BCH,ADA,UNI,REN,EOS,GRT,IOTA,AAVE,NANO,KNC,USDC,DASH,XTZ,BTT,WAVES,OXT,MKR,ALGO,FET,BTG,ZRX,ZEC,LRC,MANA,OMG,XMR,ICX,ETC,TRX,YFI,NEO,PAXG,DOGE,SC,ATOM,LINK,USDT,LUNA,SHIB,ETH,AVAX,CRV,AXS,AMP,SNX,UMA,SAND,UST,SNGLS'));
+
+    var responseBody = await jsonDecode(response.body);
+    var data = responseBody['data'];
+    List list = [];
+    await data.forEach((key, value) {
+      list.add(Coins(value));
+    });
+    var first = list[1];
+    // print('GetData Ran\n');
+    setState(() {
+      _data = list;
+      _noOfItem = list.length;
+    });
+    // print('No of Item is \n$_noOfItem');
+  }
+
+  Future again() async {
+    sleep(Duration(seconds: 10));
+    // print("Run again");
+    getData();
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getData();
+    again();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Do your Previous Works
+    // print('No of Item is \n$_noOfItem');
+    // Now Return Result
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0,
+        centerTitle: true,
+        title: Text(
+          'Crypto-Currency Prices',
+          style: TextStyle(
+            color: Colors.orange[600],
+            fontWeight: FontWeight.w400,
+            fontFamily: 'Ubuntu',
+            // fontSize: 20.0,
+          ),
+        ),
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+        child: ListView.builder(
+          itemCount: _noOfItem,
+          itemBuilder: (context, index) {
+            return HomeCard(_data[index].item['name'], _data[index].item['iso'],
+                _data[index].item['ohlc']['c']);
+          },
+        ), // child: ListView(
+        //   scrollDirection: Axis.vertical,
+        //   children: [
+        //     HomeCard(),
+        //     HomeCard(),
+        //     HomeCard(),
+        //     HomeCard(),
+        //     HomeCard(),
+        //     HomeCard(),
+        //     HomeCard(),
+        //     HomeCard(),
+        //   ],
+        // ),
+      ),
+    );
+  }
+}
+
+class HomeCard extends StatefulWidget {
+  String _coinName = "BitCoin", _isoCode = "BTC";
+  var _marketCapital;
+
+  HomeCard(this._coinName, this._isoCode, this._marketCapital);
+
+  @override
+  State<HomeCard> createState() =>
+      _HomeCardState(_coinName, _isoCode, _marketCapital);
+}
+
+class _HomeCardState extends State<HomeCard> {
+  String _coinName = "BitCoin", _isoCode = "BTC";
+  var _marketCapital;
+
+  // dynamic _indicatorColor = Colors.green[700];
+  dynamic _indicatorColor = Colors.redAccent[700];
+  // Widget percentage = PriceIncriment(0.075);
+  Widget percentage = PriceDecrement(-0.954);
+
+  _HomeCardState(this._coinName, this._isoCode, this._marketCapital);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Color.fromARGB(255, 206, 186, 238),
+      shadowColor: Colors.amberAccent[400],
+      child: InkWell(
+        splashColor: Colors.amber[300],
+        onTap: () {
+          Navigator.pushNamed(
+            context,
+            '/details',
+            arguments: {
+              'iso': this._isoCode,
+              'name': this._coinName,
+            },
+          );
+        },
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            // crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Row(
+                children: [
+                  CircleAvatar(
+                    // backgroundColor: Colors.black,
+                    backgroundImage: AssetImage(logoPath[_isoCode]!),
+                    radius: 35.0,
+                  ),
+                  SizedBox(width: 10.0),
+                  Expanded(
+                    flex: 1,
+                    // color: Colors.blue,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '${this._coinName} (${this._isoCode})',
+                          style: TextStyle(
+                            color: Colors.amber[800],
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20.0,
+                          ),
+                        ),
+                        SizedBox(height: 5.0),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              '\$$_marketCapital',
+                              style: TextStyle(
+                                color: this._indicatorColor,
+                                fontWeight: FontWeight.w600,
+                                fontSize: 18.0,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class PriceIncriment extends StatelessWidget {
+  final _value;
+  PriceIncriment(this._value);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: [
+          Icon(Icons.arrow_drop_up, color: Colors.green[700]),
+          Text(
+            '${this._value}%',
+            style: TextStyle(color: Colors.green[700], fontSize: 18.0),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class PriceDecrement extends StatelessWidget {
+  final dynamic _value;
+  const PriceDecrement(this._value, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(Icons.arrow_drop_down, color: Colors.redAccent[700]),
+        Text(
+          '$_value%',
+          style: TextStyle(color: Colors.redAccent[700], fontSize: 18.0),
+        ),
+      ],
+    );
+  }
+}
